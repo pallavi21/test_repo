23916f8fe3b63828b08593949cbcb82ccf03cbda
csolution <csolution@csolution.(none)>
2014-11-18 18:38:19 +0530

    new_git.php committed

---
+++ git_file.php
@@ -0,0 +1,59 @@
+Initializing a Repository in an Existing Directory::
+
+---> $ git init
+	 $ git add *.c
+	 $ git add LICENSE
+     $ git commit -m 'initial project version'
+     
+Cloning an Existing Repository
+
+----> git clone  https://github.com/pallavi21/test_repo /var/www/folder_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Git-Specific Commands
+
+---->git init
+	git config
+	git help
+	git status
+	git add
+	git commit -m “Message here.”
+	git branch
+	git checkout
+	git merge
+	git push
+	git pull
+	
+	
+--->Clone the remote repository
+    git clone git@github.com:user/mygit.git
+
+    Create a branch
+    git checkout -b marker_up_down
+
+    Change (edit) your file
+
+    Add and commit your file
+    git add path/main.html
+    git commit
+
+    Push the change to remote
+    git push origin marker_up_down
+
+
+
